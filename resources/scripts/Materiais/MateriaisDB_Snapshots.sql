69d9d364ed86e4ae22758a9f6c39115edbf3542e
HTAJnr <helderjuniorvucas13@gmail.com>
2026-05-31 21:37:47 +0200

    fix(snapshots): adicionar data_demissao ao SELECT de repl_funcionarios nos 3 nós

--- resources/scripts/Materiais/MateriaisDB_Snapshots.sql
+++ resources/scripts/Materiais/MateriaisDB_Snapshots.sql
@@ -26,7 +26,8 @@ CREATE MATERIALIZED VIEW repl_funcionarios
   NEXT SYSDATE + 1/24
 AS
 SELECT f.cod_funcionario, f.nome_funcionario, f.email, f.contacto,
-       f.id_funcao, f.cod_biblioteca, fn.nivel_acesso, fn.nome_funcao, f.senha
+       f.id_funcao, f.cod_biblioteca, fn.nivel_acesso, fn.nome_funcao, f.senha,
+       f.data_demissao
 FROM funcionario@link_nacionaldb f,
      funcao_funcionario@link_nacionaldb fn
 WHERE f.id_funcao = fn.id_funcao AND f.data_demissao IS NULL;
